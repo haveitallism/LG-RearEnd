8626f411a5c4b57e51a204b131497767ce1ab93c
shesaifei <2424998593@qq.com>
2022-02-21 18:11:46 +0800

    11

--- lg-gateway-8000/src/main/java/com/group8/LgGateway8000Application.java
+++ lg-gateway-8000/src/main/java/com/group8/LgGateway8000Application.java
@@ -2,15 +2,15 @@ package com.group8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
+
+/*
  * @author laiyong
  * @date 2022/2/21 15:57 星期一
  * @apiNote
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class LgGateway8000Application {
     public static void main(String[] args) {
         SpringApplication.run(LgGateway8000Application.class, args);
