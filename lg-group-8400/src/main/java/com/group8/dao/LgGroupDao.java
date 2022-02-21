8da2cf53a68a6faf472d162ac74b73d172308fc3
佘赛飞 <2424998593@qq.com>
2022-02-21 17:46:31 +0800

    11

---
+++ lg-group-8400/src/main/java/com/group8/dao/LgGroupDao.java
@@ -0,0 +1,66 @@
+package com.group8.dao;
+
+import com.group8.entity.LgGroup;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 跟团游项目(LgGroup)表数据库访问层
+ *
+ * @author makejava
+ * @since 2022-02-19 10:15:19
+ */
+public interface LgGroupDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param groupId 主键
+     * @return 实例对象
+     */
+    LgGroup queryById(Integer groupId);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
+     * @return 对象列表
+     */
+    List<LgGroup> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param lgGroup 实例对象
+     * @return 对象列表
+     */
+    List<LgGroup> queryAll();
+
+    /**
+     * 新增数据
+     *
+     * @param lgGroup 实例对象
+     * @return 影响行数
+     */
+    int insert(LgGroup lgGroup);
+
+    /**
+     * 修改数据
+     *
+     * @param lgGroup 实例对象
+     * @return 影响行数
+     */
+    int update(LgGroup lgGroup);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param groupId 主键
+     * @return 影响行数
+     */
+    int deleteById(Integer groupId);
+
+}
---
+++ lg-group-8400/src/main/java/com/group8/service/impl/LgComboServiceImpl.java
@@ -0,0 +1,47 @@
+package com.group8.service.impl;
+
+import com.group8.dao.LgComboDao;
+import com.group8.entity.LgCombo;
+import com.group8.service.LgComboService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * description: LgComboService <br>
+ * date: 2022/2/21 2:54 下午 <br>
+ * author: shesaifei <br>
+ * version: 1.0 <br>
+ */
+@Service
+public class LgComboServiceImpl implements LgComboService {
+    @Resource
+    LgComboDao lgComboDao;
+
+    @Override
+    public int insert(LgCombo lgCombo) {
+
+        return lgComboDao.insert(lgCombo);
+    }
+
+    @Override
+    public List<LgCombo> selectAll(LgCombo lgCombo) {
+        return lgComboDao.selectAll(lgCombo);
+    }
+
+    @Override
+    public int update(LgCombo lgCombo) {
+        return lgComboDao.update(lgCombo);
+    }
+
+    @Override
+    public boolean deleteById(int comboId) {
+        return lgComboDao.deleteById(comboId);
+    }
+
+    @Override
+    public LgCombo selectById(Integer comboId) {
+        return lgComboDao.selectById(comboId);
+    }
+}
